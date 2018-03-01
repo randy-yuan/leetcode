898517277c45fce69b55ff226c89ffbcfbc58122
randy-yuan <elex04@163.com>
2018-03-01 22:19:09 +0800

    Update SingleNumber.java

--- java/SingleNumber.java
+++ java/SingleNumber.java
@@ -4,6 +4,8 @@ Given an array of integers, every element appears twice except for one. Find tha
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 */
+
+// 利用XOR操作的特性: A ^ A = 0 & A ^ B = B ^ A
 class Solution {
     public int singleNumber(int[] nums) {
         int length = nums.length;
