b4b15f2d9a45f60d40a4dc32375165ca21bf5101
randy-yuan <elex04@163.com>
2018-03-01 23:42:05 +0800

    Update SingleNumber2.java

--- java/SingleNumber2.java
+++ java/SingleNumber2.java
@@ -9,16 +9,18 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 此类题目需充分挖掘条件，利用位运算特性。
 只需要对每一个bit位出现1的次数计数，出现3次清零。
 需要2bit存储计数状态，3个状态：00, 01, 10
-首先得到: ones = ones ^ A[i]; if (twos == 1) then ones = 0; 最终转换为: ones = (ones ^ A[i]) & ~twos
-类似地:  twos = twos ^ A[i]; if (ones' == 1) then twos = 0; 最终转换为: twos = (twos ^ A[i]) & ~ones
+首先得到: ones = ones ^ nums[i]; if (twos == 1) then ones = 0; 最终转换为: ones = (ones ^ nums[i]) & ~twos
+类似地:  twos = twos ^ nums[i]; if (ones' == 1) then twos = 0; 最终转换为: twos = (twos ^ nums[i]) & ~ones
 ones'就是最终的结果
 */
 
-public int singleNumber(int[] A) {
-    int ones = 0, twos = 0;
-    for(int i = 0; i < A.length; i++){
-        ones = (ones ^ A[i]) & ~twos;
-        twos = (twos ^ A[i]) & ~ones;
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
+        }
+        return ones;
     }
-    return ones;
 }
