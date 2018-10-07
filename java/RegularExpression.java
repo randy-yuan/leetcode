d06c2813d7800afa0e8f4c739b767a6f2876cf08
randy-yuan <elex04@163.com>
2018-10-07 23:17:09 +0800

    Update RegularExpression.java

--- java/RegularExpression.java
+++ java/RegularExpression.java
@@ -9,6 +9,30 @@ https://leetcode-cn.com/problems/regular-expression-matching/description/
 // 本质上和递归是一样的，算是递归算法的非递归版本，没有递归算法好理解
 // 在实际解题时，优先考虑递归算法，比较容易实现，且不容易出错
 
+/**
+// 由于问题的子结构特性，可采用动态规划算法，缓存中间结果，减少计算量
+class Solution {
+    public boolean isMatch(String text, String pattern) {
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        dp[text.length()][pattern.length()] = true;
+
+        for (int i = text.length(); i >= 0; i--){
+            for (int j = pattern.length() - 1; j >= 0; j--){
+                boolean first_match = (i < text.length() &&
+                                       (pattern.charAt(j) == text.charAt(i) ||
+                                        pattern.charAt(j) == '.'));
+                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+                } else {
+                    dp[i][j] = first_match && dp[i+1][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
+*/
+
 
 class Solution {
     enum Mode {
